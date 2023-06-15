3ab761621e76c3dd2e319f98de07f9f082ab85a5
JonasNJensen <jonas2877364@gmail.com>
2023-06-15 13:10:17 +0200

    event handler
    created event handler

---
+++ Assets/Scripts/Event/SingleParamEvent.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class SingleParamEvent : UnityEvent<object> { }
+
+public partial class EventManager
+{
+    private static Dictionary<EventName, SingleParamEvent> singleParamEvents = new();
+
+    public static void StartListening(EventName name, UnityAction<object> handler)
+    {
+        if (!singleParamEvents.TryGetValue(name, out SingleParamEvent thisEvent))
+        {
+            thisEvent = new SingleParamEvent();
+            singleParamEvents.Add(name, thisEvent);
+        }
+
+        thisEvent.AddListener(handler);
+    }
+
+    public static void StopListening(EventName name, UnityAction<object> handler)
+    {
+        if (singleParamEvents.TryGetValue(name, out SingleParamEvent thisEvent))
+        {
+            thisEvent.RemoveListener(handler);
+        }
+    }
+
+    public static void Fire(EventName name, object param)
+    {
+        if (singleParamEvents.TryGetValue(name, out SingleParamEvent thisEvent))
+        {
+            thisEvent.Invoke(param);
+        }
+    }
+}
