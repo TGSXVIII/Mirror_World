3cb932b77245ffddea82bf16f18d0ad1a4eecce9
JonasNJensen <jonas2877364@gmail.com>
2023-06-23 10:58:38 +0200

    saving/loaing of item states
    need to find a way to identify unique items

--- Assets/Scripts/Save/SaveGame.cs
+++ Assets/Scripts/Save/SaveGame.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -22,6 +23,16 @@ public class SaveGame : MonoBehaviour
         public Vector3 position;
         public List<InventoryManager.Item> inventory;
         public List<InventoryManager.Item> mirror;
+        public List<GameWorldState> items;
+    }
+
+    [System.Serializable]
+    public class GameWorldState
+    {
+        public string name;
+        public int objectID;
+        public bool isActive;
+        // Add any additional fields or data structures as needed
     }
 
     // Save the player's progress
@@ -33,7 +44,27 @@ public class SaveGame : MonoBehaviour
         data.position = player.transform.position;
         data.inventory = inventoryManager.GetInventoryData();
         data.mirror = mirrorInventory.GetInventoryData();
-        Debug.Log(data.inventory.Count);
+        GameObject[] objects = Resources.FindObjectsOfTypeAll<GameObject>();
+        Debug.Log(objects.Length);
+        List<GameWorldState> worldstate = new List<GameWorldState>(); 
+        // Iterate over each object and add its state to the Items list
+        foreach (GameObject obj in objects)
+        {
+            if (obj.CompareTag("item"))
+            {
+                GameWorldState objectState = new GameWorldState();
+                objectState.name = obj.name;
+                objectState.objectID = obj.GetInstanceID();
+                // Check if the game object is active
+                objectState.isActive = obj.activeSelf;
+                if (objectState != null)
+                {
+                    // Set other relevant state properties as needed
+                    worldstate.Add(objectState);
+                }
+            }
+        }
+        data.items = worldstate;
 
         // Serialize the data to JSON
         string json = JsonUtility.ToJson(data);
@@ -91,11 +122,28 @@ public class SaveGame : MonoBehaviour
         // Find the player object in the new scene
         inventoryManager = GameObject.FindGameObjectWithTag("inventory").GetComponent<InventoryManager>();
         mirrorInventory = GameObject.FindGameObjectWithTag("mirrorInventory").GetComponent<InventoryManager>();
+        GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
         Debug.Log(data.inventory);
         // Apply the saved data to the player
         player.transform.position = data.position;
         inventoryManager.LoadInventoryData(data.inventory);
         mirrorInventory.LoadInventoryData(data.mirror);
+        foreach (GameWorldState state in data.items)
+        {
+            foreach (GameObject obj in allObjects)
+            {
+                if (obj.GetInstanceID() == state.objectID)
+                {
+                    Debug.Log(state.name + " " + state.isActive);
+                    if (obj != null)
+                    {
+                        Debug.Log(state.name + " " + state.isActive);
+                        obj.SetActive(state.isActive);
+                        // Apply other relevant state properties to the object
+                    }
+                }
+            }
+        }
         SceneManager.UnloadSceneAsync("Main Menu");
         Debug.Log("Game loaded.");
     }
